68bf086d4668c8616d7925dfde9e5434146f6def
Dorian C <thenewdorian21@gmail.com>
2022-03-03 00:30:42 -0700

    not working

---
+++ JS projects/averjs.js
@@ -0,0 +1,21 @@
+
+
+const numArray = ["0", "1", "2", "3", "4"];
+let destination = document.querySelector("#list");
+
+let newul = document.createElement("ul");
+newul.innerHTML = `<li>${numArray[3]}</li>`;
+destination.appendChild(child);
+
+// let parent = document.querySelector("#output");
+// let child = document.createElement("tr");
+//             child.innerHTML = `<td>hgfbgfd</td><td id="points">gfgf</td>`
+//             parent.appendChild(child);
+
+function writeList() { //takes each item in numArray and makes an item in the ul
+    
+    for (let i = 0; i < numArray.length; i++) {
+       
+    }
+
+};
