6cb682d743f5598a2c50a6ee9184c4fd70746bad
Dorian Cottle <Dorian.Cottle@snow.edu>
2022-03-29 11:30:31 -0600

    added stuff

--- Vue+Bootstrap/app.js
+++ Vue+Bootstrap/app.js
@@ -1,19 +1,20 @@
 const mathprobarray = new Array("1x1=", "2x2=", "3x3=", "4x4=", "5x5=", "6x6=", "7x7=", "8x8=");
+const mathanswersheet = new Array("1", "4","9","16","25","36","49","64")
 var app = new Vue({
     el: '#app',
     data() {
         return {
-            // problem: "hello"
-            mathprob: "hi"
+            mathprobs: ["1x1=", "2x2=", "3x3=", "4x4=", "5x5=", "6x6=", "7x7=", "8x8="],
+            answers: ["1", "4","9","16","25","36","49","64"]
         };
     },
     methods: {
         submit() {
             console.log(this.input);
-            // if (this.input == answerarray[x])
-            // {
-            //     status: 
-            // }
+            if (this.input == mathanswersheet[x])
+            {
+                
+            }
         },
         getRandomInt(max) {
             return Math.floor(Math.random() * max);
@@ -22,7 +23,7 @@ var app = new Vue({
             let x = getrandomint(mathprobarray.length)
             console.log(x)
             return mathprobarray[x];
-
+    
         }
         
     }
