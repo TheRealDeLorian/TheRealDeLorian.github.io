b0e2ccf53360de17d2ac578cb0168c6b28c0c8c9
Dorian Cottle <Dorian.Cottle@snow.edu>
2022-03-29 12:13:45 -0600

    cool

--- Vue+Bootstrap/app.js
+++ Vue+Bootstrap/app.js
@@ -1,13 +1,21 @@
-var app = new Vue({
-    el: '#app',
+let app = new Vue({
+    el: "#app",
     data() {
         return {
-            mathprobs: ["1x1=", "2x2=", "3x3=", "4x4=", "5x5=", "6x6=", "7x7=", "8x8="],
-            answers: ["1", "4", "9", "16", "25", "36", "49", "64"]
-        };
+            problems: ["1x1=", "2x2=", "3x3=", "4x4=", "5x5=", "6x6=", "7x7=", "8x8="],
+            randommath: 0,
+        }
     },
+    methods: {
+        getRandomInt() {
+            let randIndex = Math.floor(Math.random() * app.problems.length);
+            this.randommath = app.problems[randIndex];
+        },
+        window.addEventListener("load", () => {
+            document.querySelector("#submit").addEventListener("click", getRandomInt)
+        });
 
-    
+}});
 
-
-});
+// problems: ["1x1=", "2x2=", "3x3=", "4x4=", "5x5=", "6x6=", "7x7=", "8x8="],
+//             answers: ["1", "4", "9", "16", "25", "36", "49", "64"]
