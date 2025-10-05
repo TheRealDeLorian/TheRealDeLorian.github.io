77fded880cee7394bfd15857c512e71fc572e68c
therealdelorian <thenewdorian21@gmail.com>
2025-10-05 08:48:24 -0600

    Fixed the bored api

--- FinalProj/Fscript.js
+++ FinalProj/Fscript.js
@@ -1,74 +1,79 @@
 let servings = 1;
-calculatebutton = document.querySelector("#calculatebutton"); /*REQUIREMENT 9 queryselector 1*/
-servingsInput = document.querySelector(".servings"); /*REQUIREMENT 9 queryselector 2*/
+calculatebutton =
+  document.querySelector("#calculatebutton"); /*REQUIREMENT 9 queryselector 1*/
+servingsInput =
+  document.querySelector(".servings"); /*REQUIREMENT 9 queryselector 2*/
 
-document.getElementById("calculatebutton").addEventListener("click", ChangeServings); /*REQUIREMENT 8 eventlistener 1*/
+document
+  .getElementById("calculatebutton")
+  .addEventListener("click", ChangeServings); /*REQUIREMENT 8 eventlistener 1*/
 function ChangeServings() {
-    if (servingsInput.value == 0 || servingsInput.value == "") {
-        ResetAmts();
-    }
-    else {
-        ResetAmts();
-        myApp.brownsugaramt *= servingsInput.value;
-        myApp.butteramt *= servingsInput.value;
-        myApp.eggamt *= servingsInput.value;
-        myApp.saltamt *= servingsInput.value;
-        myApp.bakingsodaamt *= servingsInput.value;
-        myApp.vanillaamt *= servingsInput.value;
-        myApp.flouramt *= servingsInput.value;
-        myApp.chocolatechipsamt *= servingsInput.value;
-    }
-};
+  if (servingsInput.value == 0 || servingsInput.value == "") {
+    ResetAmts();
+  } else {
+    ResetAmts();
+    myApp.brownsugaramt *= servingsInput.value;
+    myApp.butteramt *= servingsInput.value;
+    myApp.eggamt *= servingsInput.value;
+    myApp.saltamt *= servingsInput.value;
+    myApp.bakingsodaamt *= servingsInput.value;
+    myApp.vanillaamt *= servingsInput.value;
+    myApp.flouramt *= servingsInput.value;
+    myApp.chocolatechipsamt *= servingsInput.value;
+  }
+}
 
 let myApp = Vue.createApp({
-    data() {
-        return {
-            brownsugaramt: 1 * servings,
-            butteramt: 1 / 4 * servings, //make fractions fractions and not decimals
-            eggamt: 2 * servings,
-            saltamt: 1 / 2 * servings,
-            bakingsodaamt: 1 / 2 * servings,
-            vanillaamt: 1 / 2 * servings,
-            flouramt: 4 * servings,
-            chocolatechipsamt: 1 * servings,
-        }
-    }
-}).mount('#ingredientsTable')
+  data() {
+    return {
+      brownsugaramt: 1 * servings,
+      butteramt: (1 / 4) * servings, //make fractions fractions and not decimals
+      eggamt: 2 * servings,
+      saltamt: (1 / 2) * servings,
+      bakingsodaamt: (1 / 2) * servings,
+      vanillaamt: (1 / 2) * servings,
+      flouramt: 4 * servings,
+      chocolatechipsamt: 1 * servings,
+    };
+  },
+}).mount("#ingredientsTable");
 
 function ResetAmts() {
-    myApp.brownsugaramt = 1;
-    myApp.butteramt = 1 / 4;
-    myApp.eggamt = 2;
-    myApp.saltamt = 1 / 2;
-    myApp.bakingsodaamt = 1 / 2;
-    myApp.vanillaamt = 1 / 2;
-    myApp.flouramt = 4;
-    myApp.chocolatechipsamt = 1;
+  myApp.brownsugaramt = 1;
+  myApp.butteramt = 1 / 4;
+  myApp.eggamt = 2;
+  myApp.saltamt = 1 / 2;
+  myApp.bakingsodaamt = 1 / 2;
+  myApp.vanillaamt = 1 / 2;
+  myApp.flouramt = 4;
+  myApp.chocolatechipsamt = 1;
 }
 
 //API Calling
-let url = "https://www.boredapi.com/api/activity/";
-document.querySelector("#newapi").addEventListener('click', fetchdata); /*REQUIREMENT 8 eventlistener 2*/
-function fetchdata() { 
-    fetch(url) /*REQUIREMENT 10 lines 53-57 interact with an API*/
-        .then(response => response.json())
-        .then(data => {
-            document.querySelector('#insertactivityhere').innerText = data.activity;
-            activityVue.activity = data.activity;
-        })
+let url = "https://corsproxy.io/?https://bored-api.appbrewery.com/random";
+document
+  .querySelector("#newapi")
+  .addEventListener("click", fetchdata); /*REQUIREMENT 8 eventlistener 2*/
+function fetchdata() {
+  fetch(url) /*REQUIREMENT 10 lines 53-57 interact with an API*/
+    .then((response) => response.json())
+    .then((data) => {
+      document.querySelector("#insertactivityhere").innerText = data.activity;
+      activityVue.activity = data.activity;
+    });
 }
 
-let activitydesc = fetchdata()
-let activityVue = Vue.createApp({ /*EXTRA CREDIT 4 - lines 62-73 use vue*/
-    data() {
-        return {
-            activity: activitydesc,
-        }
+let activitydesc = fetchdata();
+let activityVue = Vue.createApp({
+  /*EXTRA CREDIT 4 - lines 62-73 use vue*/
+  data() {
+    return {
+      activity: activitydesc,
+    };
+  },
+  methods: {
+    fetchdatanew() {
+      fetchdata();
     },
-    methods: {
-        fetchdatanew() {
-            fetchdata();
-        }
-    }
-}).mount('#insertactivityhere')
-
+  },
+}).mount("#insertactivityhere");
